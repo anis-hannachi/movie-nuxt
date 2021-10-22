b569f430a00816068221b974b5752bcd49702789
anis.hannachi <anishannachi@debian>
2021-10-22 13:59:00 +0200

    fetching data

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,64 @@
 <template>
-  <Tutorial/>
+  <div class="home">
+    <!-- Hero -->
+    <Hero />
+    <!-- Movies -->
+    <div class="container movies">
+      <div id="movie-grid" class="movie-grid">
+        <div v-for="(movie, index) in movies" :key="index" class="movie">
+          <div class="movie-img">
+            <img
+              :src="`https://image.tmdb.org/t/p/${movie.poster_path}`"
+              alt=""
+            />
+            <p class="review">{{ movie.vote_average }}</p>
+            <p class="overview">{{ movie.overview }}</p>
+          </div>
+          <div class="info">
+            <p class="title">
+              {{movie.title.slice(0,25)}} <span v-if="movie.title.length > 25">....</span>
+            </p>
+            <p class="release">
+              Released:
+              {{
+                new Date(movie.release_date).toLocaleString('en-us', {
+                  month: 'long',
+                  day: 'numeric',
+                  year: 'numeric',
+                })
+              }}
+            </p>
+            <NuxtLink class="button button-light" :to="{name: 'movies-movieid', params: { movieid: movie.id}}">
+              Get More Info
+            </NuxtLink>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {}
+import axios from 'axios'
+export default {
+  data() {
+    return {
+      movies: [],
+    }
+  },
+  async fetch() {
+    await this.getMovies()
+  },
+  methods: {
+    async getMovies() {
+      const data = axios.get(
+        'https://api.themoviedb.org/3/movie/now_playing?api_key=afb3ef8512d0cc6df0443ab0399aa665&language=en-US&page=1'
+      )
+      const result = await data
+      result.data.results.forEach((movie) => {
+        this.movies.push(movie)
+      })
+    },
+  },
+}
 </script>
